d19df79fc5e4a99511024c505a1ea3dfb4d9224b
Skellzz <163452729+Skellzz@users.noreply.github.com>
2026-04-23 13:08:01 +0300

    sda

---
+++ Euroopa.xaml.cs
@@ -0,0 +1,13 @@
+namespace Naidis_TARpe24;
+
+public partial class Euroopa : ContentPage
+{
+	public class Telefon
+	{
+		public string Nimetus { get; set; }
+		public string Tootja { get; set; }
+		public int Hind { get; set; }
+		public string Pilt { get; set; }
+	}
+
+}
