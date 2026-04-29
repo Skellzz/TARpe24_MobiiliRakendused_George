a951b6f919baac2671144adab080f9639e929cb4
Skellzz <george.teemus@gmail.com>
2026-04-29 20:43:35 +0300

    s

---
+++ Services/LanguageService.cs
@@ -0,0 +1,22 @@
+﻿using Kolm_keelt.Resources.Localization;
+using System.Globalization;
+
+
+namespace Kolm_keelt.Services
+{
+    public static class LanguageService
+    {
+        public static event Action? LanguageChanged;
+
+        public static void ChangeLanguage(string languageCode)
+        {
+            var culture = new CultureInfo(languageCode);
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CultureInfo.DefaultThreadCurrentUICulture = culture;
+
+            AppResources.Culture = culture;
+            LanguageChanged?.Invoke();
+            Preferences.Set("AppLanguage", languageCode);
+        }
+    }
+}
